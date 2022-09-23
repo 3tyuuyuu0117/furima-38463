a8c2931fb22320ca7c45d7ae3ace0e5e0b849ac1
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-09-23 20:39:16 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,17 @@
 class Item < ApplicationRecord
   belongs_to :user
+  validates :name, presence: true
+  validates :discription, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_cost_id, presence: true
+  validates :prefecture_id, presence: true   
+  validates :shipping_date_id, presence: true   
+  validates :user, presence: true   
+
+
+    with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999},
+                      presence: { message: "can't be blank" }
+  end 
 end
