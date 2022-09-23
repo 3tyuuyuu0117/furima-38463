524bf21cfc138253f2a265060e0bca48147f4c18
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-09-23 23:21:24 +0900

    アクティブハッシュに関連するバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,15 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
   belongs_to :user
   validates :name, presence: true
   validates :discription, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :delivery_cost_id, presence: true
-  validates :prefecture_id, presence: true   
-  validates :shipping_date_id, presence: true   
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_cost_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}   
+  validates :shipping_date_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}   
   validates :user, presence: true   
-
 
     with_options presence: true, format: { with: /\A[0-9]+\z/ } do
     validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999},
