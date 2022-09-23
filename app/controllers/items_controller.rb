b9a8ae33c110ae38e3a608b8f7970e96b37aced3
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-09-23 23:22:14 +0900

    アクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,16 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
