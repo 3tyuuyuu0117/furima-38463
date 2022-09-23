6136c2537a3eb51019288678bd0df85510280efa
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-09-23 12:27:24 +0900

    テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    name                  {'test'}
+    email                 {'test@test'}
+    password              {'aaaaa1'}
+    encrypted_password    {'aaaaa1'}
+    last_name             {'山田'}
+    first_name            {'太郎'}
+    last_name_kana        {'ヤマダ'}
+    first_name_kana       {'タロウ'}
+    birthday              {'1930/01/01'}
+  end
+end
