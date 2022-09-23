b27651988837b6ddf44243fff81e602ef61f413e
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-09-23 12:26:23 +0900

    バリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :last_name_kana, presence: true
   validates :birthday, presence: true
+  validates :email, presence: true
+  validates :encrypted_password, presence: true, length: { minimum: 6 } 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+  end 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
