c0c0f8421e705e286d8151035346b3c47a0fd6ef
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-09-25 14:49:04 +0900

    モデル単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :item do
-    
+      name                  {Faker::Name.initials(number: 2)}
+      discription           {Faker::Lorem.sentence}
+      category_id           { 2 }
+      condition_id          { 2 }
+      delivery_cost_id      { 2 }
+      prefecture_id         { 3 }
+      shipping_date_id      { 2 }
+      price { 1000 }
+      association :user
+  
+      after(:build) do |item|
+    end
   end
 end
