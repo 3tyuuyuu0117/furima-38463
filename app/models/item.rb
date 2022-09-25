e48dd2d06785daf07d9cb86bddecc8a38dbd81df
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-09-25 14:49:14 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,16 +6,15 @@ class Item < ApplicationRecord
 
   validates :name, presence: true
   validates :discription, presence: true
-  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :delivery_cost_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}   
-  validates :shipping_date_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}   
   validates :user, presence: true   
   validates :image, presence: true
 
-    with_options presence: true, format: { with: /\A[0-9]+\z/ } do
-    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999},
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :shipping_date_id
+  end
+
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999},
                       presence: { message: "can't be blank" }
   end 
 end
