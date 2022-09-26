04f4bd52b483f77f8c5d6b71695a12d451e1ea60
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-09-26 16:31:51 +0900

    修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -18,7 +18,7 @@ class Item < ApplicationRecord
     validates :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :shipping_date_id
   end
 
-  with_options presence: true, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999} do
-    validates :price
+  with_options presence: true, format: { with: /\A[0-9]+\z/ } do
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is invalid'}
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -86,17 +86,17 @@ RSpec.describe Item, type: :model do
           it 'priceが300以下では登録されない' do
             @item.price = 299
             @item.valid?
-            expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+            expect(@item.errors.full_messages).to include("Price is invalid")
           end
           it 'priceが9999999より多いと登録できない' do
             @item.price = 1000000000
             @item.valid?
-            expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+            expect(@item.errors.full_messages).to include("Price is invalid")
           end
           it 'priceは半角数字以外が含まれている場合は登録できない' do
             @item.price = '１０００'
             @item.valid?
-            expect(@item.errors.full_messages).to include("Price is not a number")
+            expect(@item.errors.full_messages).to include("Price is invalid")
           end
           it 'userがなければ出品できない' do
             @item.user = nil
