33d296f7cee8cbbe874bdf019a7501b8eaa8c1b3
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-09-28 21:13:25 +0900

    アクション定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :edit]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :edit]
 
 
   def index
@@ -33,6 +33,13 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+  end
+end
 
   private
 
