c011f70d1f16b64f5d5dd7bdb89999e53e262d59
3tyuuyuu0117 <yukajn1002@gmail.com>
2022-10-04 15:09:59 +0900

    修正３

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -1,6 +1,6 @@
 class PurchaseOrder
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :purchase_id, :token
+  attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :token
 
   with_options presence: true do
     validates :user_id
@@ -17,6 +17,6 @@ end
 
   def save
   purchase = Purchase.create(user_id: user_id, item_id: item_id)
-  Order.create!(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase.id)
+  Order.create!(post_code: post_code, prefecture_id: prefecture_id, city: city, address: address, building_name: building_name, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
